07825a55a3a43cedb0096e10d347639c37a7a4d7
Alex-McClelland <alexander.cheshire@gmail.com>
2024-06-30 15:00:26 -0400

    Updated working version to 1.0.3

--- Assets/Scripts/Engine/MasterController.cs
+++ Assets/Scripts/Engine/MasterController.cs
@@ -65,7 +65,7 @@ namespace LCS.Engine
         }
 
         public const int MAX_LOG_SIZE = 200;
-        public const string CURRENT_VERSION = "1.0.2";
+        public const string CURRENT_VERSION = "1.0.3";
         private static MasterController mc = null;
 
         private Random rand;
